927f47f4534bba985d12d0e8fb08f1ebcb3bfaa2
SavageDays <low-ocean@yandex.ru>
2020-10-10 13:52:18 +0300

    Миграции

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([[
+            'name' => 'Oleg',
+            'email' => 'oleg@example.com',
+            'password' => Hash::make('password'),
+            'is_admin' => 0
+        ], [
+            'name' => 'Ivan',
+            'email' => 'ivan@example.com',
+            'password' => Hash::make('password'),
+            'is_admin' => 1
+        ]]);
+    }
+}
